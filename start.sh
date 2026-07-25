accd61e446c3a4ebfe95c45185f1953b417caa00
KarimovProject <abdullatifkarimov772@gmail.comm>
2026-07-25 19:29:23 +0500

    Fayl yuklash vaqti 120 soniyaga uzaytirildi

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-exec gunicorn --bind 0.0.0.0:8000 --workers 3 oncoscience.wsgi:application
+exec gunicorn --bind 0.0.0.0:8000 --workers 3 --timeout 120 oncoscience.wsgi:application
